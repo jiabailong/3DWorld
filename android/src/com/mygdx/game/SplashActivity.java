c88a64ee809d603e8702b6f3814ba660150f3069
jbl <1257816303@qq.com>
2018-03-09 10:54:11 +0800

    222

--- android/src/com/mygdx/game/SplashActivity.java
+++ android/src/com/mygdx/game/SplashActivity.java
@@ -10,12 +10,6 @@ import android.widget.ImageView;
 import com.mygdx.game.navigation.NavigationActivity;
 
 public class SplashActivity extends Activity{
-
-    private ImageView home;
-    private ImageView our;
-    private ImageView other;
-    private int height;
-    private int width;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,25 +20,10 @@ public class SplashActivity extends Activity{
             public void run() {
                 Intent intent=new Intent(SplashActivity.this,NavigationActivity.class);
                 SplashActivity. this.startActivity(intent);
+                SplashActivity. this.finish();
             }
         },3000);
 
     }
-    private void init(){
-        home = (ImageView) findViewById(R.id.login_home);
-        our = (ImageView) findViewById(R.id.army_our);
-        other = (ImageView) findViewById(R.id.army_other);
-        our.setOnClickListener(new MyListener());
-        other.setOnClickListener(new MyListener());
-    }
-    class MyListener implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            if(v.getId()==R.id.army_our){
 
-            }else if(v.getId()==R.id.army_our){
-
-            }
-        }
-    }
 }
--- android/src/com/mygdx/game/widget/NavigationLayout.java
+++ android/src/com/mygdx/game/widget/NavigationLayout.java
@@ -13,12 +13,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.mygdx.game.R;
+
 /**
  * Created by jbl on 18-3-8.
  */
 
 public class NavigationLayout extends LinearLayout {
     String[] names = {"主页", "军用模型", "模型数据", "使用帮助"};
+    int []icons={R.drawable.navicon_home,R.drawable.navicon_air,R.drawable.navicon_home,R.drawable.navicon_help};
     NaviItemView curview;
 
     public NavigationLayout(@NonNull Context context) {
@@ -46,41 +49,13 @@ public class NavigationLayout extends LinearLayout {
             naviItemView.setNormal();
             naviItemView.setPosotion(i);
             naviItemView.setWidgetName(names[i]);
+            naviItemView.setWidgetIcon(icons[i]);
             this.addView(naviItemView, lp);
             if (i == 0) {
                 curview = naviItemView;
                 curview.setFocus();
             }
-            naviItemView.setOnTouchListener(new OnTouchListener() {
-                @Override
-                public boolean onTouch(View view, MotionEvent arg1) {
-                    NaviItemView img = (NaviItemView) view;
-                    // int mArrayColor[] = null;
-                    int x = (int) (arg1.getX());
-                    int y = ((int) arg1.getY());
-                    Bitmap b = ((BitmapDrawable) (img.getBackground())).getBitmap();
-                    int img_height = img.getHeight();
-                    int img_width = img.getWidth();
-                    double scale_height = (double) b.getHeight() / (double) img_height;
-                    double scale_width = (double) b.getWidth() / (double) img_width;
-
-                        if (y < b.getHeight() && x > 0 && y > 0 && x < b.getWidth()) {
-                            int color = b.getPixel((int) (x * scale_width),
-                                    (int) (y * scale_height));
-                            if (Color.alpha(color) == 0) {
-                                return false;
-                            }else{
-                                curview.setNormal();
-                                curview = (NaviItemView) NavigationLayout.this.getChildAt( (((NaviItemView) view).getPosotion()));
-                                curview.setFocus();
-                                return true;
-                            }
-                        }
-
-
-                    return false;
-                }
-            });
+            naviItemView.setOnTouchListener(new ShotTouchListener());
 //            naviItemView.setOnClickListener(new OnClickListener() {
 //                @Override
 //                public void onClick(View view) {
@@ -101,6 +76,37 @@ public class NavigationLayout extends LinearLayout {
             int height = naviItemView.getHeight();
             int move_dance = (int) (0.28 * height) * (i - 1);
             naviItemView.layout(0, height * i - move_dance, width, (i + 1) * height - move_dance);
+        }
+    }
+
+    public class ShotTouchListener implements OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent arg1) {
+            NaviItemView img = (NaviItemView) view;
+            // int mArrayColor[] = null;
+            int x = (int) (arg1.getX());
+            int y = ((int) arg1.getY());
+            Bitmap b = ((BitmapDrawable) (img.getBackground())).getBitmap();
+            int img_height = img.getHeight();
+            int img_width = img.getWidth();
+            double scale_height = (double) b.getHeight() / (double) img_height;
+            double scale_width = (double) b.getWidth() / (double) img_width;
+
+            if (y < b.getHeight() && x > 0 && y > 0 && x < b.getWidth()) {
+                int color = b.getPixel((int) (x * scale_width),
+                        (int) (y * scale_height));
+                if (Color.alpha(color) == 0) {
+                    return false;
+                } else {
+                    curview.setNormal();
+                    curview = (NaviItemView) NavigationLayout.this.getChildAt((((NaviItemView) view).getPosotion()));
+                    curview.setFocus();
+                    return true;
+                }
+            }
+
+
+            return false;
         }
     }
 }
